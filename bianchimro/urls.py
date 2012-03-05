9f640c4ad62465395e25359e7e6aa6ec67d45932
Mauro Bianchi <bianchimro@gmail.com>
2012-03-05 22:30:36 +0100

    Fixed zinnia templates to include sidebar

--- bianchimro/urls.py
+++ bianchimro/urls.py
@@ -16,11 +16,13 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^blog/', include('zinnia.urls')),
+    #url(r'^weblog/', include('zinnia.urls')),    
     url(r'^comments/', include('django.contrib.comments.urls')),
     
     #cms urls
     url(r'^', include('cms.urls')),
+    
+
     
 )
 
