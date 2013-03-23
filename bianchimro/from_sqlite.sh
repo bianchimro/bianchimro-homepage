579483c27cc17db75bf384dbb38e53e3e863ecb7
Mauro Bianchi <bianchimro@gmail.com>
2013-03-23 15:16:34 +0100

    script

---
+++ bianchimro/from_sqlite.sh
@@ -0,0 +1,5 @@
+python manage.py dumpdata -n --exclude cms googlemap picture text cmsplugin_zinnia > f1.json
+python manage.py dumpdata -n cms.placeholder > f2.json
+python manage.py dumpdata -n cms googlemap picture text cmsplugin_zinnia --exclude=cms.placeholder >  f3.json
+
+
