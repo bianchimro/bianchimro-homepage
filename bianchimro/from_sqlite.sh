a4ee96b83cf28e8a7ddcdb31386b926a58064e48
Mauro Bianchi <bianchimro@gmail.com>
2013-03-24 23:46:11 +0100

    fixtures

--- bianchimro/from_sqlite.sh
+++ bianchimro/from_sqlite.sh
@@ -1,4 +1,3 @@
-python manage.py dumpdata -n --exclude cms googlemap picture text imagestore imagestore_cms cmsplugin_zinnia > f1.json
+python manage.py dumpdata -n --exclude cms > f1.json
 python manage.py dumpdata -n cms --exclude=cms.placeholder >  f2.json
 python manage.py dumpdata -n cms.placeholder > f3.json
-
